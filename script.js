690ac1db16a7101cd68dacd9228a94c7b233f493
EsraaNasr92 <esraanasr706@gmail.com>
2021-03-29 10:21:23 +0200

    Change swap function

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function myFunction(){
   var change = document.body;
   change.classList.toggle("light");
-}
 
-$("#swap").click(function () {
-    $(this).attr('src', this.src.indexOf('icon-moon.svg') > -1 ? '../images/icon-sun.svg': '../images/icon-moon.svg')
-})
+  var img_id = document.getElementById('swap');
+
+  if (img_id.src.match("../images/icon-moon.svg")) {
+      img_id.src = "../images/icon-sun.svg";
+  }
+  else {
+      img_id.src = "../images/icon-moon.svg";
+  }
+
+}
